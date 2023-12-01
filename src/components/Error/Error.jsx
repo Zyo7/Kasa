a67edfd420ed29ebd0ac9b267101ac5c4ee97d63
Zyo7 <franki.tonnelier@gmail.com>
2023-12-01 16:52:26 +0100

    Commit du CSS de la page 404

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,8 @@
 const ErrorCompo = () => (
-    <div>
-        <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
-        <a href="/">Retourner sur la page d'accueil</a>
+    <div className="errorPage">
+        <h1 className="errorPage__title">404</h1>
+        <p className="errorPage__text">Oups! La page que vous demandez n'existe pas.</p>
+        <a className="errorPage__link" href="/">Retourner sur la page d'accueil</a>
     </div>
 );
 export default ErrorCompo;
